88eb9bf800ad16cee337f00492d04012c8b421c5
SQ6ARA <piotrekb.345@gmail.com>
2025-04-26 19:51:49 +0200

    first commit

---
+++ 1.20 WDaLUR/src/main/java/github/PixelRupi/WDaLUR/WDaLUR.java
@@ -0,0 +1,9 @@
+package github.PixelRupi.WDaLUR;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(WDaLUR.MODID)
+// We Do a Little Uranium Refining :3
+public class WDaLUR {
+    public static final String MODID = "wdalur";
+}
